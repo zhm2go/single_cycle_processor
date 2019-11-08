92626081c7e1657ca111d83a7762cdaa514750dc
zhm2go <55109552+zhm2go@users.noreply.github.com>
2019-11-07 23:30:45 -0500

    Delete shift_2_bit.v

--- shift_2_bit.v
+++
@@ -1,7 +0,0 @@
-module shift_2_bit(sx, target_address);
-	input [31:0] sx;
-	output [31:0] target_address;
-	
-	assign target_address[31:2] = sx[29:0];
-	assign target_address[1:0] = 2'b00;
-endmodule
